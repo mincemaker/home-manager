524576ca6e5e684b2e1d2a0ab166994f850c14da
mincemaker <mince@hey.com>
2026-09-09 18:35:24 +0900

    fix(linux): arto ビルド時の rustc スタックオーバーフローを回避
    Dioxus/ICU4X 依存の重いジェネリクスで LLVM 並列 codegen ワーカー
    スレッドのデフォルトスタックサイズが不足し、SIGABRT/SIGSEGV で
    ビルドが確率的に失敗していたため RUST_MIN_STACK を明示指定した。

--- modules/niri/arto.nix
+++ modules/niri/arto.nix
@@ -2,6 +2,10 @@
 
 {
   home.packages = [
-    arto.packages.${pkgs.stdenv.hostPlatform.system}.default
+    # rustc の LLVM 並列 codegen ワーカースレッドがデフォルトの
+    # スタックサイズでは足りず SIGABRT/SIGSEGV で落ちることがあるため増やす
+    (arto.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
+      RUST_MIN_STACK = "67108864";
+    }))
   ];
 }
